92073894a2fcd7cdbec29fb003e728c0c31e4b1e
thewooniverse <57866696+thewooniverse@users.noreply.github.com>
2023-07-06 03:44:10 -0700

    revisions

--- python/Learning_Path_1/Part_1_Python_Fundamentals/File_IO/module.py
+++ python/Learning_Path_1/Part_1_Python_Fundamentals/File_IO/module.py
@@ -10,4 +10,5 @@ with open('example.txt', 'r') as file:
 def print_text(txt):
     print(txt)
 
-print_text(content)
+# print_text(content)
+## when you import it and this is not commented out, it will print (contet again)
