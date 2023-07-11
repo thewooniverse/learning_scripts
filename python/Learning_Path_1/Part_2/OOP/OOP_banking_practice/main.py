c8831f122e3b1557edbeca9f1f2c6c1e5a9f3a6d
thewooniverse <57866696+thewooniverse@users.noreply.github.com>
2023-07-11 05:30:11 -0700

    checkpoint before revamping add_customer and all bank methods

--- python/Learning_Path_1/Part_2/OOP/OOP_banking_practice/main.py
+++ python/Learning_Path_1/Part_2/OOP/OOP_banking_practice/main.py
@@ -16,7 +16,7 @@ class Customer:
     
 
 class Account:
-    def __init__(self, account_number, balance, customer_id):
+    def __init__(self, account_number, customer_id, balance=None):
         self.account_number = account_number
         self.balance = balance
         self.customer_id = customer_id
@@ -34,34 +34,51 @@ class Account:
         return(self.balance)
 
 class Bank:
-    def __init__(self, customers, accounts, CIDS):
+    def __init__(self, customers, accounts):
         self.customers = customers
         self.accounts = accounts
     
     def add_customer(self, name, address):
-        # randomly generate a CID that is not already in use
+        # randomly generate a Customer ID that is not already in use
         random_num = random.randint(1, 100000)
         current_datetime = datetime.now()
         formatted_date = current_datetime.strftime('%Y%m%d')
         new_customer_id = f"CID-{formatted_date}-{random_num}"
         assert new_customer_id not in [customer.customer_id for customer in self.customers]
-        # this should be optimized in the future with sorting algorithems potentially
-        # and this would spur me to try out testing / runtimes and begin learning on optimization
+        # this should be optimized in the future with sorting algorithms potentially
+        # and this would spur me to try out testing / runtimes and begin learning on optimizations!
 
-        new_customer = Customer(name, address, customer_id)
+        new_customer = Customer(name, address, new_customer_id)
         print("New customer created")
         new_customer.display_profile()
         self.customers.append(new_customer)
     
 
-    def remove_customer(self, customer_id):
+    def remove_customer(self, rm_customer_id):
+        # try to find
+        for customer in self.customers:
+            if customer.customer_id == rm_customer_id:
+                self.customers.remove(customer)
+                print(f"Removed customer: {rm_customer_id}")
+                return
         
-
-
-
-
+        # if it loops through without finding it, we print not found message and return
+        print(f"Customer: {rm_customer_id} does not exist in the system")
+        return
     
+    def create_account(self, customer):
+        # randomly generate a Account ID that is not already in use
+        random_num = random.randint(1, 100000)
+        current_datetime = datetime.now()
+        formatted_date = current_datetime.strftime('%Y%m%d')
+        new_account_id = f"AID-{formatted_date}-{random_num}"
+        assert new_account_id not in [account.account_id for account in self.accounts]
 
+        # create and add the new account into accounts
+        new_account = Account(new_account_id, customer.customer_id)
+        self.accounts.append(new_account)
+    
+    def remove_account()
 
 
 
