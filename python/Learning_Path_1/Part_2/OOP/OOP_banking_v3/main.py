c7b1b633aadfeae7241fb5a6e81f34879cc84b89
thewooniverse <57866696+thewooniverse@users.noreply.github.com>
2023-08-01 08:36:22 -0700

    general functionality with saved objects

--- python/Learning_Path_1/Part_2/OOP/OOP_banking_v3/main.py
+++ python/Learning_Path_1/Part_2/OOP/OOP_banking_v3/main.py
@@ -30,7 +30,12 @@ class Bank:
             os.mkdir(self.customers_path)
             os.mkdir(self.accounts_path)
 
-    
+    # UI methods
+
+
+
+    ## customer methods ##
+    # create customer
     def create_customer(self):
         """
         # create new customer - needs to have dialogue to create new randomized encryption key (enter PIN and encrypts it)
@@ -59,20 +64,40 @@ class Bank:
         # save the object in self.path
         with open(self.customers_path + f'{os.path.sep}{customer_id}.pkl', 'wb') as f:
             pickle.dump(new_customer, f)
-
+    
     # delete existing customer
-
 
     # change customer information
 
     # authenticate customer / access information
     ## user_id / account_id then ask for PIN
 
-    # reconciliate / check customer information
+    
+
+    ## account related methods ##
+    def create_account(self, customer):
+
+        # randomly generate account_d
+        new_account_id = "1858129"
+
+        # create a new account and add it to the customer accounts
+        new_account = Account(new_account_id)
+        customer.accounts.append(new_account_id)
 
 
 
 class Customer:
+    """
+    Customer objects are accessed by the user, through the banking UI.
+    They are stored as pickle files and their methods / functions can only be accessed if the PIN they entered is correct
+    Eventually, all of their files will be encrypted and in order to unlock them, you would need to enter the correct PIN
+
+    >> To Do's
+    > get total account sum / accounts overview
+
+    """
+
+
     def __init__(self, name, address, cid, pin):
         self.name = name
         self.address = address
@@ -82,26 +107,35 @@ class Customer:
         # PIN should be an encrypted result so that the raw object saved does not contain the actual PIN but an encrypted version
         self.pin = pin
         
-    
     # check / refresh account details after saving / transaction
 
-    # deposit (account, amount)
+    # create new account
 
-    # withdraw (account, amount)
+
+    # account_deposit (account, amount)
+
+    # account_withdraw (account, amount)
+
+    # access account
 
 
 class Account:
-    def __init__(self, account_id, balance):
+    def __init__(self, account_id):
         self.balance = 0
         self.account_id = account_id
+    
+    
 
 
-# test section
-bank = Bank("BOA")
-print(bank.name)
-bank.create_customer()
 
+# # testing bank creation
+# bank = Bank("BOA")
+# print(bank.name)
+# bank.create_customer()
 
+# testing customer file persistence
 with open(f'{os.getcwd()}{os.path.sep}BOA{os.path.sep}customers{os.path.sep}18238591.pkl', 'rb') as f:
     customer = pickle.load(f)
     print(customer.name)
+    print(customer.pin)
+    print(customer.cid)
