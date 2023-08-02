a0f30d0efb3584b47e249a1dbfc0e44828c38c20
thewooniverse <57866696+thewooniverse@users.noreply.github.com>
2023-08-02 08:44:51 -0700

    Full functionality depositing, withdrawing accounts and persistent storage of info / data across sessions

--- python/Learning_Path_1/Part_2/OOP/OOP_banking_v3/main.py
+++ python/Learning_Path_1/Part_2/OOP/OOP_banking_v3/main.py
@@ -9,12 +9,14 @@ import pickle
 
 """
 To do's:
+1. Develop UI methods
+2. Add RNG for account IDs and others
+3. Removing customer and accounts
+4. Changing details of accounts
+5. Encryption and getpass / password access and controlled
 
-
+Docstrings and tidy up.
 """
-
-
-
 
 
 
@@ -62,13 +64,11 @@ class Bank:
         print("Please enter your four digit PIN\n")
         customer_pin = input(">> ")
 
-        # randomly generate a customer ID and create the customer path
+        # randomly generate a customer ID and create the customer path + check for duplicates
         customer_id = "18238591"
-        customer_path = f"{self.customers_path}{os.path.sep}{customer_id}.pkl"
-
 
         # create the object
-        new_customer = Customer(customer_name, customer_address, customer_id, customer_pin, customer_path)
+        new_customer = Customer(customer_name, customer_address, customer_id, customer_pin, self.path)
 
         # save the object in self.path
         with open(self.customers_path + f'{os.path.sep}{customer_id}.pkl', 'wb') as f:
@@ -82,6 +82,7 @@ class Bank:
     # change customer information
     # authenticate customer / access information
 
+
     def create_account(self, target_customer_id):
         """
         To do's:
@@ -89,24 +90,11 @@ class Bank:
         make sure that they don't exist already so as to not overwrite.
         """
         # open / access the target customer object (evnetually with authenticate / login)
+        # call the create account method for the customer object
         with open(f"{self.customers_path}{os.path.sep}{target_customer_id}.pkl", 'rb') as f:
-            # randomly generate account_ID for customer
             customer_object = pickle.load(f)
-            new_account_id = "18581295921"
-
-            # create a new account and add it to the customer object's account
-            new_account = Account(new_account_id)
-            customer_object.accounts.append(new_account_id)
-
-            # save the account object in the accounts directory
-            with open(self.accounts_path + f'{os.path.sep}{new_account_id}.pkl', 'wb') as f:
-                pickle.dump(new_account, f)
+            customer_object.create_account()
         
-            # save the target customer object (reconcile)
-            with open(customer_object.path, 'wb') as old_file:
-                pickle.dump(customer_object, old_file) #dump the currently open customer object into the old file
-
-
 
 
 class Customer:
@@ -119,54 +107,140 @@ class Customer:
     > get total account sum / accounts overview
 
     """
-
-    def __init__(self, name, address, cid, pin, path):
+    def __init__(self, name, address, cid, pin, bank_path):
+        # key attributes
         self.name = name
         self.address = address
         self.accounts = [] # accounts is a list that stores account_IDs associated with this customer, at init it is empty
         self.cid = cid
-
         # PIN should be an encrypted result so that the raw object saved does not contain the actual PIN but an encrypted version
         self.pin = pin
-        self.path = path
-    
-    
-    # check / refresh account details after saving / transaction
 
+        # path variables for customer object
+        self.bank_path = bank_path
+        self.path = f"{bank_path}{os.path.sep}customers{os.path.sep}{cid}.pkl"
+        self.accounts_path = f"{bank_path}{os.path.sep}accounts"
+    
+    #### METHODS ####
+
+    def reconcile(self):
+        """
+        customer.reconcile()
+        - opens self.path and dumps itself into the path.
+        """
+        with open(self.path, 'wb') as f:
+            pickle.dump(self, f)
+        
+    
     # create new account
+    def create_account(self):
+
+        # randomly generate a new account ID
+        new_account_id = "185812959"
+
+        # append account ID to self accounts
+        self.accounts.append(new_account_id)
+
+        # create the new account object
+        new_account = Account(new_account_id, self.bank_path)
+
+        # save the account in the right directory
+        with open(self.accounts_path + f'{os.path.sep}{new_account_id}.pkl', 'wb') as f:
+            pickle.dump(new_account, f)
+        self.reconcile()
+    
 
     # account_deposit (account, amount)
+    def deposit_account(self, account_id, amount):
+        # check that the account is indeed owned by this customer
+        assert account_id in self.accounts
+        
+        # find and open the account
+        with open(f'{self.accounts_path}{os.path.sep}{account_id}.pkl', 'rb') as f:
+            account_object = pickle.load(f)
+            # call the deposit function on the account
+            account_object.deposit(amount)
 
     # account_withdraw (account, amount)
+    def withdraw_account(self, account_id, amount):
+        # check that the account is indeed owned by this customer
+        assert account_id in self.accounts
+
+        with open(f'{self.accounts_path}{os.path.sep}{account_id}.pkl', 'rb') as f:
+            account_object = pickle.load(f)
+            # call the deposit function on the account
+            account_object.withdraw(amount)
+
+
 
     # access account
 
 
 class Account:
-    def __init__(self, account_id):
+    def __init__(self, account_id, bank_path):
         # does not contain information on who owns the accounts
-
         self.balance = 0
         self.account_id = account_id
+        self.bank_path = bank_path
+        self.path = f"{bank_path}{os.path.sep}accounts{os.path.sep}{account_id}.pkl"
+    
+    def reconcile(self):
+        """
+        account.reconcile()
+        - opens self.path and dumps itself into the path.
+        """
+        with open(self.path, 'wb') as f:
+            pickle.dump(self, f)
 
+    def deposit(self, amount):
+        self.balance += amount
+        self.reconcile()
+    
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print("Withdrawal unavailable, amount exceeds current balance")
+        else:
+            self.balance -= amount
+        self.reconcile()
+        
+    
+    def check_balance(self):
+        print(f'Current balance: ${self.balance}')
+        return(self.balance)
+
+    
+
+
+
+
+
+
+
+#################################### testing ####################################
 
 
 # # testing bank creation
-bank = Bank("BOA")
+# bank = Bank("BOA")
 # print(bank.name)
 # bank.create_customer()
-bank.create_account("18238591")
+# bank.create_account("18238591")
 
 
 # testing customer file persistence
-with open(f'{os.getcwd()}{os.path.sep}BOA{os.path.sep}customers{os.path.sep}18238591.pkl', 'rb') as f:
-    customer = pickle.load(f)
-    print(customer)
-    print(customer.name)
-    print(customer.pin)
-    print(customer.cid)
-    print(customer.accounts)
+# with open(f'{os.getcwd()}{os.path.sep}BOA{os.path.sep}customers{os.path.sep}18238591.pkl', 'rb') as f:
+#     customer = pickle.load(f)
+#     print(customer)
+#     print(customer.name)
+#     print(customer.pin)
+#     print(customer.cid)
+#     print(customer.accounts)
 
+#     customer.withdraw_account('185812959', 1000)
+
+
+# with open(f'{os.getcwd()}{os.path.sep}BOA{os.path.sep}accounts{os.path.sep}185812959.pkl', 'rb') as f:
+#     account = pickle.load(f)
+#     account.check_balance()
 
 
 
