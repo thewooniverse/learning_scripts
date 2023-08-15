7a1e8422446998191ca5d4421db4131a80697c0c
thewooniverse <57866696+thewooniverse@users.noreply.github.com>
2023-08-15 01:49:19 +0100

    completed notes and practice scripts for docs(intro to data structures) + indexing tutorial from GPT

--- python/Learning_Path_1/part_3/datascience/pandas/notes_2.md
+++ python/Learning_Path_1/part_3/datascience/pandas/notes_2.md
@@ -1,6 +1,8 @@
 # Documentation: 
 https://pandas.pydata.org/docs/user_guide/index.html#user-guide
 
+https://pandas.pydata.org/docs/user_guide/indexing.html#indexing << indexing data
+
 
 
 
@@ -11,21 +13,115 @@ https://www.youtube.com/watch?v=iGFdh6_FePU&t=627s
 
 ### Notes ###
 Series is like a column of data, and a dataframe is much like a spreadsheet.
+DataFrame is a 2-dimensional labeled data structure with columns of potentially different types. 
+You can think of it like a spreadsheet or SQL table, or a dict of Series objects. It is generally the most commonly used pandas object. 
+Like Series, DataFrame accepts many different kinds of input:
 
 #### Practice questions (in practices_2.py): ####
 0. Load from the London weather series and play around with the indexes
 1. Create a new series with random numbers
+1.a. Play around with Series - indexing and slicing.
+
 2. Create a new dataframe with random numbers
-3. Practice indexing and slicing on both Series and Dataframes
-3.a. Practice on Series
-3.a.i.
-
-3.b. Practice on DataFrames
-3.b.i.
-
-4. Practice conditional statements
-
-5. Wrangle data to create a new column
+2.a.
 
 
+
+
+
+
+
+PRACTICE ALL OF THE BELOW METHODS
+
+Indexing and slicing are fundamental operations in pandas for selecting and manipulating data. There are several ways to index and slice a DataFrame:
+
+1. **By Columns**:
+   - Using column labels:
+     ```python
+     df['ColumnName']
+     ```
+
+   - Using multiple columns:
+     ```python
+     df[['Column1', 'Column2']]
+     ```
+
+2. **By Rows** using `loc` and `iloc`:
+   - `loc` is primarily label based indexing:
+     ```python
+     df.loc[row_label]
+     df.loc[row_start:row_end]
+     ```
+
+   - `iloc` is primarily integer based indexing:
+     ```python
+     df.iloc[row_index]
+     df.iloc[start_index:end_index]
+     ```
+
+3. **By Rows and Columns**:
+   - Using `loc`:
+     ```python
+     df.loc[row_label, 'ColumnName']
+     df.loc[row_start:row_end, ['Column1', 'Column2']]
+     ```
+
+   - Using `iloc`:
+     ```python
+     df.iloc[row_index, column_index]
+     df.iloc[start_index:end_index, start_column:end_column]
+     ```
+
+4. **Boolean Indexing**:
+   - Based on condition:
+     ```python
+     df[df['ColumnName'] > value]
+     ```
+
+   - Multiple conditions:
+     ```python
+     df[(df['Column1'] > value1) & (df['Column2'] < value2)]
+     ```
+
+5. **Using `isin` for Filtering**:
+   - Filtering based on list of values:
+     ```python
+     df[df['ColumnName'].isin([value1, value2])]
+     ```
+
+6. **Using the `query` Method**:
+   - Useful for complex conditions:
+     ```python
+     df.query("Column1 > value1 & Column2 < value2")
+     ```
+
+7. **Using `xs` for Cross Section**:
+   - Selecting data at a particular level of a MultiIndex DataFrame:
+     ```python
+     df.xs(key=value, level='LevelName')
+     ```
+
+8. **Using `at` and `iat` for Faster Access**:
+   - `at` allows fast access to a single value using row and column labels:
+     ```python
+     df.at[row_label, 'ColumnName']
+     ```
+
+   - `iat` allows fast access to a single value using row and column integer-positions:
+     ```python
+     df.iat[row_index, column_index]
+     ```
+
+9. **Slicing with `head` and `tail`**:
+   - Get the first `n` rows:
+     ```python
+     df.head(n)
+     ```
+
+   - Get the last `n` rows:
+     ```python
+     df.tail(n)
+     ```
+
+These are just the basic ways to index and slice data in a pandas DataFrame. As you work with pandas, you'll find that it provides an extensive set of tools and methods to facilitate a wide range of data selection and manipulation tasks.
 
--- python/Learning_Path_1/part_3/datascience/pandas/practices_2.py
+++ python/Learning_Path_1/part_3/datascience/pandas/practices_2.py
@@ -12,15 +12,436 @@ weather_df = pd.read_csv(f'{os.getcwd()}{os.path.sep}london_weather.csv')
 
 
 # 1. Create a new series with random numbers
-random_series = pd.Series(np.random.randn(10))
-print(random_series)
+# random_series = pd.Series(np.random.randn(10))
+# print(random_series)
 
 random_series = pd.Series(np.random.randn(10), index=['a','b','c','d','e','f','g','h','i','j'])
-print(random_series)
+print(random_series) # prints another dataframe with same structure, but with the index as a-j
+
+# simple slicing and indexing of series
+# print(random_series[0]) # prints the first row, value only
+# print(random_series[:1]) # prints the first row, value and index
+# print(random_series[:3]) # prints the first to third row (0,1,2) or in this case a,b,c
+# print(random_series[[0,1,2,3]]) # prints rows a,b,c,d
+
+# accessing elements based on index like a dictionary (or row name)
+# print(random_series['c']) 
+random_series['c'] = 13 # reassigns value of row c
+# print(random_series) # value of c = 13
+
+# slicing based on conditionals
+# print(random_series[random_series>random_series.median()]) # prints the columns where the row value is bigger than the median value
+
+
+# other slicing / targeting and indexing
+# print(random_series.array) # prints the actual array backing the series
 
 
 
-# 2. Create a new dataframe with random numbers
+
+
+# 2. Create a new dataframe with random numbers in different ways, with different data formats and ways of constructing!
+# constructing DataFrames from Dict of Series or Dicts
+data = {
+    "age": pd.Series([50, 35, 95, 100, 69], index=['becky', 'susan', 'maccy', 'ronald', 'donald']),
+    "gender": pd.Series(["female", "female", "female", "male", "male"], index=['becky', 'susan', 'ronald', 'millie', 'donald'])
+}
+df = pd.DataFrame(data) # index of the dataframe (each row is indexed by the name of the person)
+df.index.name = "Names" # changes the name of the index column to Names
+print(df)
+"""
+OUTPUT:
+          age  gender
+Names                  <<< names was added only after df.index.name = "Names" was executed
+becky    50.0  female
+donald   69.0    male
+maccy    95.0     NaN
+millie    NaN    male
+ronald  100.0  female
+susan    35.0  female
+
+The assignments were all done without issue and the final table is a Union of the columns with missing values being assigned NaN
+"""
+
+df_2 = pd.DataFrame(data, index=['becky', 'susan', 'ronald', 'nancy'], columns=['age', 'location'])
+print(df_2)
+"""
+OUTPUT:
+          age location
+becky    50.0      NaN
+susan    35.0      NaN
+ronald  100.0      NaN
+nancy     NaN      NaN
+
+The dataframe will only take the rows where the indexes and columns defined in the construction, once again filling empty datasets with NaN.
+"""
+print(df_2.columns)
+print(df_2.index)
+"""
+columns = Index(['age', 'location'], dtype='object')
+index = Index(['becky', 'susan', 'ronald', 'nancy'], dtype='object')
+"""
+
+
+"""
+constructing from dict of ndarrays or lists
+--
+The ndarrays must all be the same length. If an index is passed, it must also be the same length as the arrays. 
+If no index is passed, the result will be range(n), where n is the array length.
+"""
+
+data_2 = {
+    "name": ['becky', 'susan', 'jimmy', 'carter'],
+    "age": [10, 12, 15, 18],
+    'height': [180, 130, 155, 180],
+    'weight': [65, 45, 50, 100]
+}
+
+df_3 = pd.DataFrame(data_2)
+print(df_3)
+"""
+OUTPUT:
+
+     name  age  height  weight
+0   becky   10     180      65
+1   susan   12     130      45
+2   jimmy   15     155      50
+3  carter   18     180     100
+
+the index is a range(n) with n as the array length in the above table, however after I df.set_index() as below, it becomes this:
+        age  height  weight
+name                       
+becky    10     180      65
+susan    12     130      45
+jimmy    15     155      50
+carter   18     180     100
+"""
+df_3.set_index('name', inplace=True) # usually, this would return a new list, but inplace=True makes it replace inplace
 
 
 
+
+
+
+
+"""
+### selecting and manipulating columns ###
+--
+You can treat a DataFrame semantically like a dict of like-indexed Series objects. 
+Getting, setting, and deleting columns works with the same syntax as the analogous dict operations:
+"""
+
+print(df_3['weight'])
+"""
+OUTPUT:
+name
+becky      65
+susan      45
+jimmy      50
+carter    100
+"""
+
+# you can create new columns and run operations to create new columns as well
+df_3['bmi'] = (df_3['weight'] / (df_3['height'] * 2)) * 100
+df_3['overweight'] = df_3['bmi'] > 25
+print(df_3)
+"""
+OUTPUT:
+        age  height  weight        bmi  overweight
+name                                              
+becky    10     180      65  18.055556       False
+susan    12     130      45  17.307692       False
+jimmy    15     155      50  16.129032       False
+carter   18     180     100  27.777778        True
+
+By default, columns get inserted at the end. DataFrame.insert() inserts at a particular location in the columns:
+"""
+
+# deleting / popping columns like a dictionary
+del df_3['overweight']
+bmi = df_3.pop('bmi') # in this case, this is a series
+print(df_3) # after both of these operations, df_3 no longer has the bmi and overweight columns
+
+
+# changing the ordering of the columns
+df_3 = df_3[['height', 'weight', 'age']]
+print(df_3)
+"""
+OUTPUT: reordered dataframe
+
+        height  weight  age
+name                       
+becky      180      65   10
+susan      130      45   12
+jimmy      155      50   15
+carter     180     100   18
+"""
+
+
+
+
+
+
+
+
+
+"""
+### INDEXING / SELECTION ###
+More in depth covered here - https://pandas.pydata.org/docs/user_guide/indexing.html#indexing
+
+--
+
+                                       syntax    returns
+operation                                               
+select column                         df[col]     Series
+select row by label             df.loc[label]     Series
+select row by integer location   df.iloc[loc]     Series
+slice rows                           df[5:10]  DataFrame
+select rows by boolean vector    df[bool_vec]  DataFrame
+
+
+loc:
+
+It stands for label-based indexing.
+You use it with labels/indices to select rows or columns.
+It can accept boolean data to filter rows.
+When slicing, both the start and stop of the range are included.
+
+iloc:
+
+It stands for integer-location based indexing.
+You use it with integer-based positions to select rows or columns.
+Doesn't consider the DataFrame's index or column labels for selection.
+When slicing, it follows standard Python slicing rules where the start is inclusive and the stop is exclusive.
+
+Basically ilocs are for like integer based stuff where you want to slice shit up using their location
+Whereas location is more label based and conditional based
+"""
+
+# indexing using label based indexing
+print(df_3.loc['susan']) # select rows simply by the index value
+"""
+OUTPUT:
+
+height    130
+weight     45
+age        12
+Name: susan, dtype: int64
+"""
+
+# loc[:] slicing based on index
+print(df_3.loc['becky':'jimmy'])
+print(df_3.loc['susan':'becky']) # << this would be useful for stuff like if index was time and we wanted to get rows of specific times
+"""
+       height  weight  age
+name                      
+becky     180      65   10
+susan     130      45   12
+jimmy     155      50   15
+
+second statement outputs:
+Empty DataFrame
+Columns: [height, weight, age]
+Index: []
+"""
+
+
+# indexing based on integer based indexing
+print(df_3.iloc[0:2]) # prints the first two rows, 0,1 which is becky and susan
+
+
+print(df_3[0:2]) # this also prints the first two rows, 0 and 1
+
+"""
+Both `df_3.iloc[0:2]` and `df_3[0:2]` will indeed yield the first two rows of the dataframe `df_3`. However, they are based on different underlying mechanics:
+
+1. **`df_3.iloc[0:2]`**:
+   - This uses `iloc`, which stands for integer-location based indexing. 
+   It allows you to select rows (or columns) at particular positions in the dataframe using integer indices.
+   - You explicitly specify that you want to select rows using integer indices.
+
+2. **`df_3[0:2]`**:
+   - This is a basic slicing technique using the DataFrame's default row indexer.
+   - Pandas DataFrame directly supports slicing on rows without any additional method. 
+   The syntax is similar to how you'd slice a Python list. This is essentially a shorthand for row slicing.
+
+Despite them yielding the same result in this context, they operate on different principles. 
+`iloc` is more general and can also be used for column indexing when you provide two arguments (one for row and one for column). 
+On the other hand, the direct slicing (`df_3[0:2]`) is specifically for rows and cannot be used directly for columns. 
+
+In practice, when you're working with rows, both methods will get the job done. 
+But it's helpful to be aware of these nuances, especially when your tasks become more complex or when you want to be more explicit in your code.
+
+TLDR --> iloc is used more for complex ones where we want to slice the whole dataframe rather than just rows
+"""
+
+
+
+
+"""
+ROWS AND COLUMNS
+"""
+print(df_3.loc['becky':'susan', ['height', 'weight']]) # label based
+print(df_3.iloc[0:3, 1:3]) # integer location based
+"""
+OUTPUT:
+       height  weight
+name                 
+becky     180      65
+susan     130      45
+
+OUTPUT 2:
+       weight  age
+name              
+becky      65   10
+susan      45   12
+jimmy      50   15
+"""
+
+
+"""
+boolean based indexing
+"""
+
+# single condition
+drinking_age = df_3[df_3['age'] > 15]
+print(drinking_age)
+print(df_3['age'] > 15) # < the conditional inside returns a boolean vector
+
+"""
+OUTPUT:
+        height  weight  age
+name                       
+carter     180     100   18
+
+
+OUTPUT2:
+
+name
+becky     False
+susan     False
+jimmy     False
+carter     True
+Name: age, dtype: bool
+"""
+
+
+# multiple conditions
+## repopulating the two columns previously removed
+df_3['bmi'] = (df_3['weight'] / (df_3['height'] * 2)) * 100
+df_3['overweight'] = df_3['bmi'] > 25
+
+overweight_and_legal_to_drink = (df_3['age'] > 15) & (df_3['bmi'] > 25)
+print(overweight_and_legal_to_drink)
+print(df_3[overweight_and_legal_to_drink]) # << using the boolean vector directly after constructing it
+"""
+OUTPUT 1:
+
+name
+becky     False
+susan     False
+jimmy     False
+carter     True
+dtype: bool
+
+
+
+OUTPUT 2:
+
+        height  weight  age        bmi  overweight
+name                                              
+carter     180     100   18  27.777778        True
+"""
+
+
+
+
+
+"""
+Querying for complex conditions << not needed for now, master the above first boyyyyy
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# d = {
+#     "one": pd.Series([1.0, 2.0, 3.0], index=["a", "b", "c"]),
+#     "two": pd.Series([1.0, 2.0, 3.0, 4.0], index=["a", "b", "c", "d"])
+# }
+# df = pd.DataFrame(d)
+# print(df.index)
+# print(df.columns)
+
+
+
+
+
+### Test snippets ###
+# Sample DataFrame
+data = {
+    'A': [1, 2, 3],
+    'B': [4, 5, 6],
+    'C': [7, 8, 9]
+}
+
+df = pd.DataFrame(data)
+# print(df)
+
+# Setting 'A' as the index
+df = df.set_index('A')
+# print(df)
+# Accessing using the index
+# print(df.loc[1])  # Using .loc to access the row with index labeled 1
+
+"""
+The original data has a default index
+   A  B  C
+0  1  4  7
+1  2  5  8
+2  3  6  9
+
+When transformed such that A becomes the index...
+   B  C
+A      
+1  4  7 <<<
+2  5  8
+3  6  9
+
+Such that when we ask for df.loc[1], it returns the row values for B and C where the index value=1
+B    4
+C    7
+"""
+indexing_df = pd.DataFrame({
+    "operation": ['select column', 'select row by label', 'select row by integer location', 'slice rows', 'select rows by boolean vector'],
+    'syntax': ['df[col]', 'df.loc[label]', 'df.iloc[loc]', 'df[5:10]', 'df[bool_vec]'],
+    "returns": ['Series', 'Series', 'Series', 'DataFrame', 'DataFrame']
+})
+indexing_df.set_index('operation', inplace=True)
+# print(indexing_df)
