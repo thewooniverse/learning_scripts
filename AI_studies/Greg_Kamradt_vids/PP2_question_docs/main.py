7d0e4309101c3b31c6dc824baa64480324247881
thewooniverse <57866696+thewooniverse@users.noreply.github.com>
2023-10-18 18:03:32 +0100

    NOTE+PRACTICE: new notes and practice on retrievalQA chain

--- AI_studies/Greg_Kamradt_vids/PP2_question_docs/main.py
+++ AI_studies/Greg_Kamradt_vids/PP2_question_docs/main.py
@@ -10,6 +10,8 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 # Model and chain
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
+from langchain.chains import RetrievalQA
+
 
 # Text splitters
 from langchain.text_splitter import CharacterTextSplitter
@@ -29,32 +31,51 @@ OPENAI_API_KEY = os.getenv('OPENAI_API_KEY', 'YourAPIKey')
 embeddings = OpenAIEmbeddings(disallowed_special=(), openai_api_key=OPENAI_API_KEY)
 llm = ChatOpenAI(temperature=0.5, model_name='gpt-4', openai_api_key=OPENAI_API_KEY)
 
-# construct the document embeddings
-root_dir = './documentations/thefuzz'
-documents = []
 
-# Go through each folder
-for dirpath, dirnames, filenames in os.walk(root_dir):
+# # construct the document embeddings
+# root_dir = './documentations/thefuzz'
+# documents = []
+
+# # Go through each folder
+# for dirpath, dirnames, filenames in os.walk(root_dir):
     
-    # Go through each file
-    for file in filenames:
-        try: 
-            # Load up the file as a doc and split
-            # print (file)
-            loader = TextLoader(os.path.join(dirpath, file), encoding='utf-8')
-            documents.extend(loader.load_and_split())
-        except Exception as e: 
-            pass
+#     # Go through each file
+#     for file in filenames:
+#         try: 
+#             # Load up the file as a doc and split
+#             # print (file)
+#             loader = TextLoader(os.path.join(dirpath, file), encoding='utf-8')
+#             documents.extend(loader.load_and_split())
+#         except Exception as e: 
+#             pass
 
-print (f"You have {len(documents)} documents\n")
-# print ("------ Start Document ------")
-# print (documents[0].page_content[:300])
-
-
+# print (f"You have {len(documents)} documents\n")
+# # print ("------ Start Document ------")
+# # print (documents[0].page_content[:300])
 
 
 
+# db = Chroma.from_documents(documents, embeddings, persist_directory="./chroma_db")
+db = Chroma(persist_directory="./chroma_db", embedding_function=embeddings)
 
+retriever = db.as_retriever()
+
+qa_chain = RetrievalQA.from_chain_type(llm, retriever=retriever, verbose=True)
+query = "Who is Joseph Mourinho"
+result = qa_chain.run(query)
+print(result)
+
+
+"""
+
+QUERY: What is the Fuzz?
+`thefuzz` is a Python library that provides functions for fuzzy string matching and similarity calculations. 
+It is used for tasks such as string matching, searching, and deduplication. 
+The library offers various algorithms and methods to calculate the similarity between strings, 
+allowing users to find matches even when there are slight differences or variations in the strings. 
+It is commonly used in applications like search engines, autocomplete suggestions, data deduplication, 
+data cleaning and preprocessing, natural language processing, and spell checking and correction.
+"""
 
 
 
---
+++ AI_studies/langchain_sophia_yang/README.md
@@ -0,0 +1,8 @@
+🤩 Is LangChain the easiest way to work with LLMs? It's an open-source tool and recently added ChatGPT Plugins. It provides so many capabilities that I find useful. Check out my video to learn more:
+
+[LangChain Overview video](https://www.youtube.com/watch?v=kmbS6FDQh7c)
+
+
+😎 Do you want to chat with your long PDF docs? And do you know that there are at least 4 ways to do question answering in LangChain? 
+
+[LangChain QA video](https://www.youtube.com/watch?v=DXmiJKrQIvg)
