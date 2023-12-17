0dead7a9aa7a4f4f4439222220fe58c21ef6d515
thewooniverse <57866696+thewooniverse@users.noreply.github.com>
2023-12-17 15:56:43 +0800

    FEATURE: basic querying ChatGPT 4 commands functional and updated todos / specs

--- AI_studies/practice_projects/tele_gpt/README.md
+++ AI_studies/practice_projects/tele_gpt/README.md
@@ -1,6 +1,10 @@
 # Overview
 TeleGPT is a simple ChatGPT integrated Telegram Bot with persistent memory and context awareness.
 TeleGPT can be configured to each user's OpenAI API Key and use different chat agents as required (GPT 3.5 and 4.0 supported).
+TeleGPT is built on two large libraries: Langchain and Telebot with Chroma based persistent storage RAG for persistent context awareness.
+
+To use Telebot, you may simply start a chat with @telegptwastaken_bot on Telegram or invite it to your group; after providing your API Key you should have access.
+
 
 ## Features and Commands
 /chat "query" - returns context aware responses to the query from the chat agent
@@ -8,6 +12,26 @@ TeleGPT can be configured to each user's OpenAI API Key and use different chat a
 /chat_history "query" - returns the documents and queries and response from the persistent chat history that best match the query
 /api_key "api_key" - enters the API key for the given chat group
 /settings - opens the settings and configuration for the given chat environment
+/add_system_message - adds system message / instructions to the bot
+
+
+In order to have multiple persistent / categorized chat histories with the bot, you may start several different groups with the agent inside it.
+For example, you may have one with:
+- Translate TeleGPT
+- Cooking TeleGPT
+
+
+
+### Dev todos
+1. Complete simple message sending / response telegram bots -> completed 
+2. Integrate simple version of querying chat agent via Telegram bot /chat "query" feature -> completed
+3. Formatting of the query responses - Markdown -> completed
+-- completed above --
+4. Settings, configurations and API keys // buttons
+5. API key provisions and entering
+6. Saving chat history to chat stores
+7. RAG based on chat history
+8. Clearing chat history
 
 
 
--- AI_studies/practice_projects/tele_gpt/telebot_main.py
+++ AI_studies/practice_projects/tele_gpt/telebot_main.py
@@ -1,13 +1,88 @@
 
 
-# importing modules
+### importing modules ###
+import os
+from dotenv import load_dotenv
+
+## Telegram + formatting libraries
+import telebot
+
+## OpenAI libraries
+import openai
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage, SystemMessage, AIMessage
+from msg_templates import system_template
+
+# Vector Support
+from langchain.vectorstores import Chroma
+from langchain.embeddings.openai import OpenAIEmbeddings
 
 
-# importing API Keys and setting constants
+
+### importing API Keys and setting constants ###
+load_dotenv()
+TG_API_KEY = os.getenv('TG_API_KEY')
+OPENAI_API_KEY = os.getenv('OPEN_AI_API_KEY')
+# print(OPEN_API_KEY) # prints 'test ABC' correctly
+# print(TG_API_KEY)
+
+
+bot = telebot.TeleBot(TG_API_KEY)
+embeddings = OpenAIEmbeddings(disallowed_special=(), openai_api_key=OPENAI_API_KEY)
+default_agent = ChatOpenAI(model='gpt-4-0613', openai_api_key=OPENAI_API_KEY, model_name="gpt-4-0613")
 
 
 
-# Defining functions
+### Defining Telegram Bot functions and decorators ###
+
+# Defining a command handler
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, f"Hello! I'm here to help! {bot.get_my_name().name}")
+    # all the configurations / guide tutorial strings here
+
+
+
+# Defining the query command handler
+@bot.message_handler(commands=['chat'])
+def send_chat_response(message):
+    # split the query:
+    query = " ".join(message.text.split()[1:])
+    context_aware_chat_history = construct_chat_history(query)
+    response = chat_agent(context_aware_chat_history)
+    bot.reply_to(message, response.content, parse_mode='Markdown')
+
+
+
+### Defining ChatGPT query related functionality ###
+def chat_agent(chat_history, agent=default_agent):
+    """
+    Calls the Chat Model to give a response to the given chat history.
+    """
+    response = agent(chat_history)
+    return response
+
+def construct_chat_history(query, context=""):
+    """
+    Returns: chat_history object that contains SystemMessage, AIMessage and HumanMessage
+    context - a plain string containing the summarized retrieved context from the ChromaDB based on the query; default is an empty string.
+    query - a plain string that was asked to the agent
+    """
+    system_message = system_template + context
+    
+    chat_history = [
+        SystemMessage(content=system_message),
+        HumanMessage(content=query)
+    ]
+    return chat_history
+
+
+
+
+
+
+### Starting the bot ###
+bot.polling()
 
 
 
