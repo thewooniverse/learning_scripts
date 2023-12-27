a541086dc6796d909eb5dea1804140315ca71890
thewooniverse <57866696+thewooniverse@users.noreply.github.com>
2023-12-27 19:24:30 +0800

    INIT: initialized / gotten started on search agent GPT

--- AI_studies/practice_projects/README.md
+++ AI_studies/practice_projects/README.md
@@ -2,6 +2,9 @@ Project workflow:
 
 1. Persistent Agent -> chat agent with chat history, retrieval and basically ChatGPT with actually persistent memory storage
 2. TeleGPT -> Telegram integration of ChatGPT, building on top of Persistent Agent; having chat history for each chat room.
-3. SearchAgentGPT -> Agent that queries itself, if it is beyond cutoff date it will search the web for the best answer;
+3. SearchAgentGPT -> Agent that queries itself, if it is beyond cutoff date it will search the web for the best answer; then build onto TeleGPT for this type of querying as well.
+4. ImageSearchGPT -> Agent that can search the images provided; and build on top of TeleGPT with this as well.
 
+--- The milestone here would definitely be in having a very robust, feature rich and reliable ChatGPT interface on telegram, which I can use to develop learning paths and stuff like that and always, at any moment query.
+I could also then use this to create a type of "AI council" of sorts! And also begin to promote it to a certain degree.
 
---
+++ AI_studies/practice_projects/searchGPT/main.py
@@ -0,0 +1,40 @@
+# Module and Library imports
+
+from dotenv import load_dotenv
+import os
+
+import openai
+from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage, SystemMessage, AIMessage
+
+# Vector Support
+# from langchain.vectorstores import FAISS >> instead of FAISS we will use Chroma
+from langchain.vectorstores import Chroma
+from langchain.embeddings.openai import OpenAIEmbeddings
+
+# Text loading and splitting
+from langchain.docstore.document import Document
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.summarize import load_summarize_chain
+
+
+# agents + toolkits
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+
+
+# env variables, constants and API keys
+load_dotenv()
+openai_api_key=os.getenv('OPENAI_API_KEY', 'YourAPIKey_BACKUP')
+serpapi_api_key=os.getenv("SERPAPI_API_KEY", "YourAPIKey_Backup")
+# print(serpapi_api_key) # check
+
+# loading toolkits
+llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
+toolkit = load_tools(["serpapi"], llm=llm, serpapi_api_key=serpapi_api_key)
+
+
+agent = initialize_agent(toolkit, llm, agent="zero-shot-react-description", verbose=True, return_intermediate_steps=True)
+response = agent({"input""What is Y Combinator and how does it work?"})
+print(response)
