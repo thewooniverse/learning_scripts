c6b22461fd9f83103abd295b69f1c790e642df94
thewooniverse <57866696+thewooniverse@users.noreply.github.com>
2023-12-29 22:58:58 +0800

    openai keys changed

--- AI_studies/practice_projects/searchGPT/main.py
+++ AI_studies/practice_projects/searchGPT/main.py
@@ -34,7 +34,6 @@ serpapi_api_key=os.getenv("SERPAPI_API_KEY", "YourAPIKey_Backup")
 llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
 toolkit = load_tools(["serpapi"], llm=llm, serpapi_api_key=serpapi_api_key)
 
-
 agent = initialize_agent(toolkit, llm, agent="zero-shot-react-description", verbose=True, return_intermediate_steps=True)
-response = agent({"input""What is Y Combinator and how does it work?"})
+response = agent({"What is the meaning of life?"})
 print(response)
