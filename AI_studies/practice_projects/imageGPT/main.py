437ec07959bc25cb0298451439abbd7ce64c00f9
thewooniverse <57866696+thewooniverse@users.noreply.github.com>
2024-01-01 23:56:47 +0900

    NOTE: notes on learning openai vs langchain

--- AI_studies/practice_projects/imageGPT/main.py
+++ AI_studies/practice_projects/imageGPT/main.py
@@ -2,6 +2,7 @@
 
 from dotenv import load_dotenv
 import os
+import pyperclip
 
 import openai
 from langchain.llms import OpenAI
@@ -13,15 +14,27 @@ from langchain.schema import HumanMessage, SystemMessage, AIMessage
 from langchain.vectorstores import Chroma
 from langchain.embeddings.openai import OpenAIEmbeddings
 
-# Text loading and splitting
-from langchain.docstore.document import Document
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.chains.summarize import load_summarize_chain
+# Image generation:
+from langchain.chains import LLMChain
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.utilities.dalle_image_generator import DallEAPIWrapper
+
 
 
 # env variables, constants and API keys
 load_dotenv()
 openai_api_key=os.getenv('OPENAI_API_KEY', 'YourAPIKey_BACKUP')
 serpapi_api_key=os.getenv("SERPAPI_API_KEY", "YourAPIKey_Backup")
-print(serpapi_api_key) # check
-print(openai_api_key)
+# print(serpapi_api_key) # check
+# print(openai_api_key)
+
+
+# Functions for testing image generation / DALL-E - This I also need to learn with OpenAI module as Langchain module / docs is outdated
+## in most cases, learning OpenAI module directly is more useful than learning only langchain - as langchain is in the end a wrapper for OpenAI and other language model APIs.
+
+# Functions for testing image based querying (AI Vision) - for this, I must use OpenAI module
+
+
+
+
