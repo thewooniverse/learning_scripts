83a49a12709cbfbae02d0e00f0ef7964dfbbf2b3
thewooniverse <57866696+thewooniverse@users.noreply.github.com>
2024-01-10 23:59:11 +0900

    NOTE: new notes on response objects

--- AI_studies/OpenAI/basics/practice_1.py
+++ AI_studies/OpenAI/basics/practice_1.py
@@ -83,5 +83,32 @@ https://platform.openai.com/docs/api-reference/chat
 
 """
 /// WALKTRHOUGH - https://platform.openai.com/docs/guides/text-generation ///
+
+
+Chat completion example:
+{
+  "choices": [
+    {
+      "finish_reason": "stop",
+      "index": 0,
+      "message": {
+        "content": "The 2020 World Series was played in Texas at Globe Life Field in Arlington.",
+        "role": "assistant"
+      },
+      "logprobs": null
+    }
+  ],
+  "created": 1677664795,
+  "id": "chatcmpl-7QyqpwdfhqwajicIEznoc6Q47XAyW",
+  "model": "gpt-3.5-turbo-0613",
+  "object": "chat.completion",
+  "usage": {
+    "completion_tokens": 17,
+    "prompt_tokens": 57,
+    "total_tokens": 74
+  }
+}
+
+
 """
 
